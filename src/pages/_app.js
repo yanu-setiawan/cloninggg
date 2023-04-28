91afcac1abb961007c93c4954e1e6c55711853b7
yanu-setiawan <yanusetiawan777@gmail.com>
2023-04-28 16:29:49 +0700

    Setup Next

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,33 @@
-import '@/styles/globals.css'
+import "styles/globals.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "react-toastify/dist/ReactToastify.css";
+
+import { Mulish } from "next/font/google";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import store, { persistor } from "redux/store";
+import { ToastContainer, Flip } from "react-toastify";
+
+const mulish = Mulish({
+  subsets: ["latin", "latin-ext"],
+  weight: ["200", "300", "400", "600", "700", "800", "900"],
+  variable: "--font-mulish",
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className={`${mulish.variable}`}>
+      <Provider store={store}>
+        <PersistGate persistor={persistor} loading={null}>
+          <Component {...pageProps} />
+          <ToastContainer
+            position="top-right"
+            autoClose={3000}
+            transition={Flip}
+            theme="colored"
+          />
+        </PersistGate>
+      </Provider>
+    </div>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        mulish: ["var(--font-mulish)", "sans-serif"],
+      },
+      backgroundImage: {},
+      colors: {
+        tickitz: {
+          primary: "3D405B",
+          secondary: "F4F1DE",
+          warning: "F2CC8F",
+          error: "E07A5F",
+          success: "81B29A",
+          darkTitle: "#14142B",
+          label: "#6E7191",
+          basic: "#4E4B66",
+        },
       },
     },
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [],
+  },
+};
